0d42dfca4440f444bbdd56f75d4fe671086dba77
Farkhadov95 <farkhadov950405@gmail.com>
2024-04-20 23:07:27 +0500

    added city cards

---
+++ src/components/CityList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div v-for="city in savedCities" :key="city.id">
+    <CityCard :city="city" @click="goToCityView(city)" />
+  </div>
+  <p v-if="savedCities.length === 0">
+    No locations added. To start tracking a location, search in the field above.
+  </p>
+</template>
+
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+import CityCard from "./CityCard.vue";
+import { useRouter } from "vue-router";
+
+const savedCities = ref([]);
+const router = useRouter();
+
+const getCities = async () => {
+  if (localStorage.getItem("savedCities")) {
+    savedCities.value = JSON.parse(localStorage.getItem("savedCities"));
+    const requests = [];
+
+    savedCities.value.forEach((city) => {
+      requests.push(
+        axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${city.coords.lat}&lon=${city.coords.lon}&appid=8d57e60fee44778a09b34670a18882a5&units=metric`
+        )
+      );
+    });
+
+    const weatherData = await Promise.all(requests);
+    weatherData.forEach((value, index) => {
+      savedCities.value[index].weather = value.data;
+    });
+  }
+};
+
+await getCities();
+
+const goToCityView = (city) => {
+  router.push({
+    name: "cityView",
+    params: { state: city.state, city: city.city },
+    query: {
+      lat: city.coords.lat,
+      lon: city.coords.lon,
+    },
+  });
+};
+</script>
