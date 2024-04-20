1a4706ba6c5763e6f92c291183206d0b8dc32ece
Farkhadov95 <farkhadov950405@gmail.com>
2024-04-20 12:36:11 +0500

    added error handling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,13 +14,19 @@
         class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
         v-if="mapboxSearchResults"
       >
-        <li
-          v-for="searchResult in mapboxSearchResults"
-          :key="searchResult.id"
-          class="py-2 cursor-pointer"
-        >
-          {{ searchResult.properties.place_formatted }}
-        </li>
+        <p v-if="searchError">Sorry, something went wrong. Please try again.</p>
+        <p v-if="!searchError && mapboxSearchResults.length === 0">
+          No results match your query, try a different term.
+        </p>
+        <template v-else>
+          <li
+            v-for="searchResult in mapboxSearchResults"
+            :key="searchResult.id"
+            class="py-2 cursor-pointer"
+          >
+            {{ searchResult.properties.place_formatted }}
+          </li>
+        </template>
       </ul>
     </div>
   </main>
@@ -35,15 +41,20 @@ const apiToken =
 const searchQuery = ref("");
 const queryTimeout = ref(null);
 const mapboxSearchResults = ref(null);
+const searchError = ref(null);
 
 const getSearchResults = () => {
   clearTimeout(queryTimeout.value);
   queryTimeout.value = setTimeout(async () => {
     if (searchQuery.value !== "") {
-      const res = await axios.get(
-        `https://api.mapbox.com/search/geocode/v6/forward?q=${searchQuery.value}&access_token=${apiToken}&types=place`
-      );
-      mapboxSearchResults.value = res.data.features;
+      try {
+        const res = await axios.get(
+          `https://api.mapbox.com/search/geocode/v6/forward?q=${searchQuery.value}&access_token=${apiToken}&types=place`
+        );
+        mapboxSearchResults.value = res.data.features;
+      } catch {
+        searchError.value = true;
+      }
       return;
     }
     mapboxSearchResults.value = null;
